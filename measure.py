1b7db98024973df621647ec222ead071d0ec3e8e
Daphne <daphnezhou@nuralogix.ai>
2019-08-29 15:01:14 -0400

    Add register license and User login to get token

--- measure.py
+++ measure.py
@@ -1,5 +1,7 @@
 import argparse
 import asyncio
+import requests
+import json
 
 from dfxsnippets.addData import addData
 from dfxsnippets.createMeasurement import createMeasurement
@@ -9,9 +11,11 @@ from dfxsnippets.websocketHelper import WebsocketHandler
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DFX API Python snippets example")
 
-    parser.add_argument("studyID", help="StudyID")
-    parser.add_argument("token", help="user or device token")
-    parser.add_argument("payloadDir", help="Directory of payload files")
+    parser.add_argument("--studyID", help="StudyID")
+    parser.add_argument("--licenseKey", help="License Key")
+    parser.add_argument("--username", help="UserName")
+    parser.add_argument("--password", help="Password")
+    parser.add_argument("--payloadDir", help="Directory of payload files")
     parser.add_argument("--restUrl",
                         help="DFX API REST url",
                         default="https://qa.api.deepaffex.ai:9443")
@@ -23,16 +27,47 @@ if __name__ == "__main__":
                         help="Connection method",
                         choices=["REST", "Websocket"],
                         default="REST")
+    parser.add_argument("--mode", help="Mode", default="DISCRETE")
 
     args = parser.parse_args()
 
     studyID = args.studyID
-    token = args.token
+    license_key = args.licenseKey
+    username = args.username
+    password = args.password
     rest_url = args.restUrl
     ws_url = args.wsUrl
     conn_method = args.connectionMethod
     input_directory = args.payloadDir
     output_directory = args.outputDir
+    mode = args.mode
+
+    # Register License and get device token
+    data = {
+            "Key": "{}".format(license_key),
+            "DeviceTypeID": "DARWIN",
+            "Name": "dfx-api-python-client",
+            "Identifier": "api-test",
+            "Version": "1.0"
+            }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(rest_url+'/organizations/licenses', data=json.dumps(data), headers=headers)
+    index1 = response.text.find('Token')
+    index2 = response.text.find('\",\"UserID')
+    deviceToken = response.text[index1+8:index2]
+    print("Device Token: ", deviceToken)
+
+    # Login and get user token
+    userdata = {
+                "Email": "{}".format(username),
+                "Password": "{}".format(password)
+                }
+    userheaders = {"Content-Type": "application/json", "Authorization": "{}".format(deviceToken)}
+    userResposne = requests.post(rest_url+'/users/auth', data=json.dumps(userdata), headers=userheaders)
+    userToken = userResposne.text[10:-2]
+    print("User Token: ", userToken)
+
+    token = userToken
 
     # Create the async event loop
     loop = asyncio.get_event_loop()
@@ -41,7 +76,7 @@ if __name__ == "__main__":
     websocketobj = WebsocketHandler(token, ws_url)
 
     # Create a measurement object and get a measurement ID
-    createmeasurementObj = createMeasurement(studyID, token, rest_url)
+    createmeasurementObj = createMeasurement(studyID, token, rest_url, mode)
     measurementID = createmeasurementObj.create()
 
     # Create an addData object (which prepares the data need to be sent in the input_directory)
@@ -66,6 +101,6 @@ if __name__ == "__main__":
     tasks.append(loop.create_task(adddataObj.sendAsync()))
     tasks.append(loop.create_task(subscriberesultsObj.subscribe()))
 
-    wait_tasks = asyncio.wait(tasks)
+    wait_tasks = asyncio.wait(tasks) 
     loop.run_until_complete(wait_tasks)
     loop.close()
